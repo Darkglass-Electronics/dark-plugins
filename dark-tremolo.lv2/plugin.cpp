7fcb086f4e0cac871d9024a36b5fbc181b1e02bd
Mikael Ruohonen <mikael@darkglass.com>
2025-10-07 14:12:37 +0300

    dark-tremolo depth smoothing and reset specifics

--- dark-tremolo.lv2/plugin.cpp
+++ dark-tremolo.lv2/plugin.cpp
@@ -52,6 +52,7 @@ struct State {
 	t_sample m_smth_3;
 	t_sample m_smth_4;
 	t_sample m_smth_wet;
+	t_sample m_smth_depth;
 	t_sample wet;
 	// re-initialize all member variables;
 	inline void reset(t_param __sr) {
@@ -76,12 +77,13 @@ struct State {
 		t_sample wrap_3 = wrap(expr_1089, ((int)0), ((int)1));
 		t_sample sin_24 = sin(expr_1090);
 		t_sample clamp_25 = ((sin_24 <= ((t_sample)1e-05)) ? ((t_sample)1e-05) : ((sin_24 >= ((t_sample)0.99999)) ? ((t_sample)0.99999) : sin_24));
-		t_sample mul_15 = (m_depth_6 * ((t_sample)0.01));
-		t_sample mul_859 = (m_depth_6 * ((t_sample)0.005));
-		t_sample add_667 = (mul_859 + ((int)1));
 		// the main sample loop;
 		while ((__n--)) {
 			const t_sample in1 = (*(__in1++));
+			m_smth_depth = (m_depth_6 + (((t_sample)0.999) * (m_smth_depth - m_depth_6)));
+			t_sample mul_15 = (m_smth_depth * ((t_sample)0.01));
+			t_sample mul_859 = (m_smth_depth * ((t_sample)0.005));
+			t_sample add_667 = (mul_859 + ((int)1));
 			t_sample mix_1 = (m_shape_5 + (((t_sample)0.999) * (m_smth_4 - m_shape_5)));
 			t_sample mix_2 = (wrap_3 + (((t_sample)0.999) * (m_smth_3 - wrap_3)));
 			t_sample mix_wet = (wet + (((t_sample)0.999) * (m_smth_wet - wet)));
@@ -114,13 +116,14 @@ struct State {
 		t_sample wrap_3 = wrap(expr_1089, ((int)0), ((int)1));
 		t_sample sin_24 = sin(expr_1090);
 		t_sample clamp_25 = ((sin_24 <= ((t_sample)1e-05)) ? ((t_sample)1e-05) : ((sin_24 >= ((t_sample)0.99999)) ? ((t_sample)0.99999) : sin_24));
-		t_sample mul_15 = (m_depth_6 * ((t_sample)0.01));
-		t_sample mul_859 = (m_depth_6 * ((t_sample)0.005));
-		t_sample add_667 = (mul_859 + ((int)1));
 		// the main sample loop;
 		while ((__n--)) {
 			const t_sample in1 = (*(__in1++));
 			const t_sample in2 = (*(__in2++));
+			m_smth_depth = (m_depth_6 + (((t_sample)0.999) * (m_smth_depth - m_depth_6)));
+			t_sample mul_15 = (m_smth_depth * ((t_sample)0.01));
+			t_sample mul_859 = (m_smth_depth * ((t_sample)0.005));
+			t_sample add_667 = (mul_859 + ((int)1));
 			t_sample mix_1 = (m_shape_5 + (((t_sample)0.999) * (m_smth_4 - m_shape_5)));
 			t_sample mix_2 = (wrap_3 + (((t_sample)0.999) * (m_smth_3 - wrap_3)));
 			t_sample mix_wet = (wet + (((t_sample)0.999) * (m_smth_wet - wet)));
@@ -179,19 +182,23 @@ struct State {
 		void* ptrs[11];
 	} lv2 = {};
 	inline void lv2_reset() {
+		// memory reset
 		m_y_1 = ((int)0);
 		m_y_2 = ((int)0);
-		m_smth_3 = ((int)0);
-		m_smth_4 = ((int)0);
-		m_smth_wet = ((int)0);
+		__m_phasor_10.reset(0);
+		// set editable to targets -> skip any smoothing
+		m_smth_wet = wet;
+		m_smth_depth = m_depth_6;
+		m_smth_4 = m_shape_5;
 	}
 	inline void lv2_prerun() {
 		wet = *lv2.ports.ctrls[0] > 0.5f ? 1.f : 0.f;
-		if (*lv2.ports.ctrls[1] > 0.5f)
-			lv2_reset();
 		set_rate(*lv2.ports.ctrls[2]);
 		set_shape(*lv2.ports.ctrls[3]);
 		set_depth(*lv2.ports.ctrls[4]);
+		// reset after parameter setting so that values jump directly to targets
+		if (*lv2.ports.ctrls[1] > 0.5f)
+			lv2_reset();
 	}
 	inline void lv2_run_mono(uint32_t nsamples) {
 		lv2_prerun();
